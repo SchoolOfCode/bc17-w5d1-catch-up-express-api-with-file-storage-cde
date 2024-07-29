83ef4beeac6634b963d64221269a07a30c7d33d0
Crom <daffodil.luck9@gmail.com>
2024-07-29 11:55:21 +0100

    update recipe By ID

--- recipes.js
+++ recipes.js
@@ -30,7 +30,26 @@ export async function createRecipe(newRecipe) {
 }
 
 // UPDATE A RECIPE BY ID
-export async function updateRecipeByID(id, updatedRecipe) {}
+export async function updateRecipeByID(id, updatedRecipe) {
+	const allRecipes = await fs.readFile(fileName, { encoding: "utf8" }); // got the allRecipes file
+	const allRecipesJSON = JSON.parse(allRecipes); // converted this into a JSON object from a string so we can manipulate it.
+	// console.log(allRecipesJSON);
+
+	let recipeIndex = allRecipesJSON.findIndex((i) => i.id === id);
+
+	allRecipesJSON[recipeIndex] = updatedRecipe;
+
+	const allRecipesString = JSON.stringify(allRecipesJSON); // converting the JSON object into a string so we can write it back in L28
+    fs.writeFile(fileName, allRecipesString, { encoding: "utf8" }); //writing back to the file
+    
+	return updatedRecipe;
+}
 
 // DELETE A RECIPE BY ID
-export async function deleteRecipeByID(id) {}
+export async function deleteRecipeByID(id) { }
+
+
+console.log(await updateRecipeByID("4c848d48-b81e-4d6f-b45d-7b3090f4f8ef", "apple pie"));
+
+
+// {"id":"4c848d48-b81e-4d6f-b45d-7b3090f4f8ef","title":"Beans on Toast","ingredients":["150g of beans","150g of butter","150g of toast"],"instructions":"Put the butter in your mouth, wait 2 seconds to allow slight melting. Then follow with the toast. Swish around for 10-15 seconds to allow even coating of butter on the toast. Then add the beans, slowly.\n  \n    Season to taste.","image":"https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x375.jpg"}
