3f205ecb799bc7e66635bb5720122ed5edf48d6b
dom-m17 <dominicmaynard00@gmail.com>
2024-07-29 12:32:15 +0100

    adding get listeners

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get('/recipes', async (req, res) => {
+  try {
+    const allRecipes = await getRecipes();
+    res.status(200).json({ success: true, payload: allRecipes })
+  } catch(error) {
+    res.status(500).json({ success: false, payload: error })
+  }
+})
+
+app.get('/recipes/:id', async (req, res) => {
+  try {
+    const recipe = await getRecipeByID(req.params.id);
+    res.status(200).json({ success: true, payload: recipe })
+  } catch(error) {
+    res.status(500).json({ success: false, payload: error })
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -1,4 +1,5 @@
 import fs from "node:fs/promises";
+import { receiveMessageOnPort } from "node:worker_threads";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "recipes.json";
@@ -6,7 +7,8 @@ const fileName = "recipes.json";
 // GET ALL RECIPES
 export async function getRecipes() {
     const allRecipes = await fs.readFile(fileName, { encoding: 'utf8' })
-    return allRecipes
+    const allRecipesJSON = JSON.parse(allRecipes);
+    return allRecipesJSON
 }
 
 // GET A RECIPE BY ID
@@ -46,10 +48,18 @@ export async function updateRecipeByID(id, updatedRecipe) {
 }
 
 // DELETE A RECIPE BY ID
-export async function deleteRecipeByID(id) { }
-
-
-console.log(await updateRecipeByID("4c848d48-b81e-4d6f-b45d-7b3090f4f8ef", "apple pie"));
+export async function deleteRecipeByID(id) {
+    const allRecipes = await fs.readFile(fileName, { encoding: "utf8" }); // got the allRecipes file
+	const allRecipesJSON = JSON.parse(allRecipes); // converted this into a JSON object from a string so we can manipulate it.
+    // console.log(allRecipesJSON)
+    let recipeIndex = allRecipesJSON.findIndex((i) => i.id === id); // Find the index of the recipe to delete
+    const deletedRecipe = allRecipesJSON[recipeIndex] // save that recipe to a variable so we can return it
+    allRecipesJSON.splice(recipeIndex, recipeIndex) // remove the recipe from the array
+    // console.log(allRecipesJSON)
+    const allRecipesString = JSON.stringify(allRecipesJSON); // converting the JSON object into a string so we can write it back in L28
+    fs.writeFile(fileName, allRecipesString, { encoding: "utf8" }); //writing back to the file
+    return deletedRecipe
+}
 
 
 // {"id":"4c848d48-b81e-4d6f-b45d-7b3090f4f8ef","title":"Beans on Toast","ingredients":["150g of beans","150g of butter","150g of toast"],"instructions":"Put the butter in your mouth, wait 2 seconds to allow slight melting. Then follow with the toast. Swish around for 10-15 seconds to allow even coating of butter on the toast. Then add the beans, slowly.\n  \n    Season to taste.","image":"https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x375.jpg"}
