97326dcb6e34b8a43b04221e3e4f3f7ae15f1ef4
Eric-0805 <ericroberts805@gmail.com>
2024-07-29 11:25:11 +0100

    create new recipe

--- recipes.js
+++ recipes.js
@@ -18,7 +18,16 @@ export async function getRecipeByID(id) {
 }
 
 // CREATE A RECIPE
-export async function createRecipe(newRecipe) {}
+export async function createRecipe(newRecipe) {
+    const allRecipes = await fs.readFile(fileName, { encoding: 'utf8' }); // got the allRecipes file
+    const allRecipesJSON = JSON.parse(allRecipes); // converted this into a JSON object from a string so we can manipulate it.
+    // console.log(allRecipesJSON)
+    allRecipesJSON.push(newRecipe); // added a newRecipe to the allRecipesJSON object
+    // console.log(allRecipesJSON);
+    const allRecipesString = JSON.stringify(allRecipesJSON); // converting the JSON object into a string so we can write it back in L28
+    fs.writeFile(fileName, allRecipesString, { encoding: 'utf8' }) //writing back to the file
+    return newRecipe; // returning the new recipe
+}
 
 // UPDATE A RECIPE BY ID
 export async function updateRecipeByID(id, updatedRecipe) {}
