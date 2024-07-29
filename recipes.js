b42f4d1b12d948f594dbd37e874961b953755478
dom-m17 <dominicmaynard00@gmail.com>
2024-07-29 10:50:37 +0100

    Completing get functions

--- recipes.js
+++ recipes.js
@@ -4,10 +4,18 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+    const allRecipes = await fs.readFile(fileName, { encoding: 'utf8' })
+    return allRecipes
+}
 
 // GET A RECIPE BY ID
-export async function getRecipeByID(id) {}
+export async function getRecipeByID(id) {
+    const allRecipes = await fs.readFile(fileName, { encoding: 'utf8' });
+    const allRecipesJSON = JSON.parse(allRecipes);
+    let recipeIndex = allRecipesJSON.findIndex(i => i.id === id); 
+    return allRecipesJSON[recipeIndex]
+}
 
 // CREATE A RECIPE
 export async function createRecipe(newRecipe) {}
